854df9d00b42e6c75294f8e4126676e3120c3448
Calvo <igorcalvob@gmail.com>
2024-03-08 00:05:02 -0300

    btop on startup

--- startup.sh
+++ startup.sh
@@ -4,6 +4,7 @@ screen -d -m steam -silent
 screen -d -m discord
 screen -d -m autokey
 screen -d -m /home/calvo/.local/bin/ahk_x11.AppImage /home/calvo/Code/Scripts/key-bindings.ahk
+screen -d -m tilix
 
 sleep 2
 audio_process=$(pgrep pavucontrol)
@@ -14,11 +15,16 @@ xdotool windowminimize `xdotool search --pid $audio_process | tail -1`
 # xdotool windowminimize `xdotool search --pid $yt_process | tail -1`
 
 sleep 5
-discord_process=$(pgrep Discord | head -n1)
-xdotool windowminimize `xdotool search --pid $discord_process | tail -1`
+discord_window=$(xdotool search --name Friends)
+xdotool windowminimize $discord_window
 
-xdotool mousemove 3000 500
-xdotool click 1
-xdotool windowminimize `xdotool search --pid $audio_process | tail -1`
+tlx=$(pgrep tilix)
+tlx=$(xdotool search --pid $tlx | tail -1)
+sleep 1
+xdotool set_desktop_for_window $tlx 0
+xdotool windowsize $tlx 33% 100%
+xdotool windowmove $tlx 0 0
+xdotool mousemove 940 45
+xdotool click --repeat 2 1
 
 exit
