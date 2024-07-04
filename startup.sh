45ec2ffcade5f49d4734e4949f3c9a47f128a4f4
igorcalvo <igorcalvob@gmail.com>
2024-07-03 23:40:16 -0300

    volume notification

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ sleep 3
 screen -d -m tilix
 screen -d -m steam -silent
 screen -d -m youtube-music
-screen -d -m discord --start-minimized
+# screen -d -m discord --start-minimized
 screen -d -m python ~/code/scripts/hourly.py
 
 # youtube-music
