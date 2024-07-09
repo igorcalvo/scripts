9491f8556ede1d627b869f7e0fb09912f156d4de
igorcalvo <igorcalvob@gmail.com>
2024-07-09 18:01:00 -0300

    fixed bug and cs2 logic

--- hourly.py
+++ hourly.py
@@ -9,19 +9,24 @@ script_path = "~/.config/polybar/nightlight.sh"
 first_run = True
 late_at_night = True
 
+def cs2_is_running() -> bool:
+    cmd = os.popen("pgrep cs2")
+    res = cmd.read()
+    return len(res) > 1
+
 while True:
     hour = datetime.datetime.now().hour
 
-    if hour < 18:
+    if hour < 18 and not hour < 7:
         os.system(f"sh {script_path} -")
         time.sleep(1)
         os.system(f"sh {script_path} -")
 
-    if hour >= 18 and first_run:
+    if hour >= 18 and first_run and not cs2_is_running():
         first_run = False
         os.system(f"sh {script_path} +")
 
-    if hour < 6 and late_at_night:
+    if hour < 6 and late_at_night and not cs2_is_running():
         late_at_night = False
         os.system(f"sh {script_path} +")
         time.sleep(1)
