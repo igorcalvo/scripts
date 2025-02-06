2128d66fa26b899aea1022237e5c481d8e427726
igorcalvo <igorcalvob@gmail.com>
2025-02-06 17:40:48 -0300

    dynamic id for reset-mouse.laptop

--- reset-mouse.laptop.sh
+++ reset-mouse.laptop.sh
@@ -1,3 +1,4 @@
-xinput --set-prop 10 'libinput Accel Profile Enabled' 0, 1
-xinput --set-prop 10 'libinput Accel Speed' -0.5
+id=$(xinput list | awk '/Razer Razer DeathAdder Essential/ && /slave  pointer/ {print $7}' | tail -n 1 | sed 's/id=//')
+xinput --set-prop ${id} 'libinput Accel Profile Enabled' 0, 1
+xinput --set-prop ${id} 'libinput Accel Speed' -0.5
 
