9e191694bb306db3f29e85d4bd4e099977669974
igorcalvo <igorcalvob@gmail.com>
2025-05-14 11:12:51 -0300

    reset mouse universal

--- cs-out.sh
+++ cs-out.sh
@@ -1,4 +1,4 @@
 xrandr --output DP-0 --auto --primary
 xrandr --output HDMI-0 --auto
-nvidia-settings --assign CurrentMetaMode="DP-0: 1920x1080_240 +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, HDMI-0: 1920x1080_75 +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
+nvidia-settings --assign CurrentMetaMode="DP-0: 1920x1080_240 +1920+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}, HDMI-0: 1920x1080_240 +0+0 {ForceCompositionPipeline=On, ForceFullCompositionPipeline=On}"
 
